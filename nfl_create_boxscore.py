5ed83b2c89f3342f29a51fd50096db5e930f2d1e
Declan <declan.farley3@gmail.com>
2025-11-09 08:12:07 -0600

    fixed boxscore stats, still adding some stats

--- nfl_create_boxscore.py
+++ nfl_create_boxscore.py
@@ -3,35 +3,47 @@ import os
 
 def create_weekly_boxscore_csv(year: int, week: int, output_file: str) -> None:
     """Creates a CSV file containing the NFL boxscore data for a specific game. 
-    This will process a week at a time, a single run will result in the creation of many CSV files, one for each game within the week.
+    This will process a week at a time, a single run will result in the creation of a single CSV file.
 
     Args:
         year (int): year for which the data represents
         week (int): week for which the data represents of the provided year
-        output_file (str): file name for the output CSV file, root of the overall file name, extra will be game_id
+        output_file (str): file name for the output CSV file
     """
     # using pbp data here instead of weekly since weekly does not include kicking stats 
     data_dir = "data/pbp_data"
     csv_file = f"nfl_pbp_{year}.csv"
-    # fetch the appropriate yearly data from our own CSV files
     yearly_data = pd.read_csv(os.path.join(data_dir, csv_file))
     # filter the data for the specified week
     weekly_data = yearly_data[yearly_data['week'] == week]
+    # add some relevant columns for boxscore stats we want to track
+    weekly_data['rushing_tds'] = ((weekly_data['td_team'] == weekly_data['posteam']) & (weekly_data['play_type'] == 'run')).astype(int)
+    weekly_data['receiving_tds'] = ((weekly_data['td_team'] == weekly_data['posteam']) & (weekly_data['play_type'] == 'pass')).astype(int)
+    weekly_data["off_td"] = (weekly_data['td_team'] == weekly_data['posteam']).astype(int)
+    weekly_data["def_td_allowed"] = ((weekly_data['td_team'] != weekly_data['posteam']) & (weekly_data['td_team'].notnull())).astype(int)
     # now we are going to create the boxscore stats for the overall team based off the play by play data
     # group by team and aggregate relevant statistics
     boxscore_stats = {
         'passing_yards': 'sum',
         'rushing_yards': 'sum',
+        'rushing_tds': 'sum',
         'receiving_yards': 'sum',
-        'touchdown': 'sum',
+        'receiving_tds': 'sum',
+        'off_td': 'sum',
+        'def_td_allowed': 'sum',
+        'two_point_conv_result' : lambda x : (x == 'success').sum(),
         'interception': 'sum',
         'fumble_lost': 'sum',
-        'field_goal_result': 'sum',
+        'field_goal_result': lambda x : (x == 'made').sum(),
         'field_goal_attempt': 'sum',
-        'extra_point_result': 'sum',
-        'extra_point_attempt': 'sum'
+        'extra_point_result': lambda x : (x == 'good').sum(),
+        'extra_point_attempt': 'sum',
+        'posteam_score' : 'max'
     }
-    weekly_data = weekly_data.groupby(['home_team', 'away_team']).agg(boxscore_stats).reset_index()
+
+    weekly_data = weekly_data.groupby(['old_game_id_x', 'posteam']).agg(
+                boxscore_stats
+            ).reset_index()
     # save the weekly boxscore data to a CSV file
     weekly_data.to_csv(output_file, index=False)
     
